167992d5613c696aad2216bb6df1b9fed5599101
manoj.gaikwad <manoj.gaikwad@sumasoft.net>
2024-08-20 12:42:34 +0530

    update password funtionality added and small changes.

---
+++ TaskManagementSystem/Models/UpdatePasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagementSystem.Models
+{
+    public class UpdatePasswordModel
+    {
+        public string Email {  get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
