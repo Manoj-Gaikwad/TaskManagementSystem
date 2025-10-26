e9944f3145868868d8c5fd0d37d162ff7fdd9256
Manoj Gaikwad <manoj.gaikwad@fusionstak.com>
2025-10-26 19:34:49 +0530

    Added Extention Methods

---
+++ TaskManagementSystem/StatupExtentions/ApplicationServicesExtention.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskManagementSystem.IRepository;
+using TaskManagementSystem.Repository;
+
+namespace TaskManagementSystem.StatupExtentions
+{
+    public static class ApplicationServicesExtention
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services
+                .AddTransient<ITaskRepository, TaskRepository>()
+                .AddTransient<IEmployeeRepository, EmployeeRepository>()
+                .AddTransient<IAdminRepository, AdminRepository>()
+                .AddSingleton<ISessionData, SessionData>()
+                .AddScoped<EmailService>();
+                
+
+        }
+    }
+}
---
+++ TaskManagementSystem/StatupExtentions/CorsPolicyExtention.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskManagementSystem.StatupExtentions
+{
+    public static class CorsPolicyExtention
+    {
+      public static IServiceCollection AddCustomCorsPolicy(this IServiceCollection services)
+      {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader()
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod();
+
+
+                });
+            });
+
+            return services;
+      }
+    }
+}
